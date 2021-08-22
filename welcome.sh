bfa0786afb819a3ac115b36ff42f00e696ab4df1
Olufunso <olufunsocoker@gmail.com>
2021-08-22 08:35:22 +0100

    Getting it right

---
+++ welcome.sh
@@ -0,0 +1,7 @@
+echo Please enter your name
+read name
+echo Please enter your secret number
+read secretnumber
+echo Correct
+echo You are welcome, Engineer
+echo Please wait for your Director to set your permissions 
